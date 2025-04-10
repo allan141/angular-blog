e4d962ac05d5912a3f12e89d44a9f46c7d746ef1
allan141 <allancavalcante141@gmail.com>
2025-04-10 17:15:05 -0300

    update

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,14 @@
-import { enableProdMode } from '@angular/core';
 import { bootstrapApplication } from '@angular/platform-browser';
 import { AppComponent } from './app/app.component';
-import { appConfig } from './app/app.config';
+import { provideRouter } from '@angular/router';
+import { routes } from './app/app.routes';
+import { importProvidersFrom } from '@angular/core';
+import { CommonModule, HashLocationStrategy, LocationStrategy } from '@angular/common';
 
-
-bootstrapApplication(AppComponent, appConfig)
-  .catch(err => console.error(err));
+bootstrapApplication(AppComponent, {
+  providers: [
+    provideRouter(routes),
+    importProvidersFrom(CommonModule),
+    { provide: LocationStrategy, useClass: HashLocationStrategy }
+  ]
+}).catch(err => console.error(err));
